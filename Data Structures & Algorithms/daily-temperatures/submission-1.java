395c9a69f7c542337e5498f635e43b86f2ea90db
Khan Aqsa Khurshid Alam <aqsakkhan04@gmail.com>
2026-09-16 12:21:34 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int result[] = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            int j = i + 1;
+            while (j < n) {
+                if (temperatures[j] > temperatures[i]) {
+                    result[i] = j - i;
+                    break;
+                }
+                j++;
+            }
+        }
+
+        return result;
+    }
+}
